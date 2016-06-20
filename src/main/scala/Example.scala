e36c21d500680bf0df750258312443e71a926d1d
Max Dube <maxdube11@gmail.com>
2016-06-20 18:56:18 -0400

    adjust for postgres

--- src/main/scala/Example.scala
+++ src/main/scala/Example.scala
@@ -14,18 +14,5 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 
 object Example extends App {
-  // connection info for a pre-populated throw-away, in-memory db for this demo, which is freshly initialized on every run
-  val url = "jdbc:h2:mem:test;INIT=runscript from 'src/main/sql/create.sql'"
-  val db = Database.forURL(url, driver = "org.h2.Driver")
-
-  // Using generated code. Our Build.sbt makes sure they are generated before compilation.
-  val q = Companies.join(Computers).on(_.id === _.manufacturerId)
-    .map { case (co, cp) => (co.name, cp.name) }
-
-  Await.result(db.run(q.result).map { result =>
-    println(result.groupBy { case (co, cp) => co }
-      .mapValues(_.map { case (co, cp) => cp })
-      .mkString("\n")
-    )
-  }, 60 seconds)
+    //
 }
